4670d8dae36e76f5b738646a243402bd5a8770d2
Maddula <venkata-sai-kumar.maddula@capgemini.com>
2023-09-23 16:52:50 +0530

    commit from 2nd pc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Home from './home/Home'
 import './App.css'
-import NavBar from './components/NavBar'
+import NavBar from './components/navbar/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -3,9 +3,18 @@ import PlayArrow from '@mui/icons-material/PlayArrow';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import "./features.css";
 
-function Features() {
+function Features({type}) {
   return (
     <div className="features">
+      {type && (<div className="category">
+        <span>{type==="movie"?"Movies":"Series"}</span>
+        <select name="genre" id="genre">
+          <option>Genre</option>
+          <option value="adventure">Adventure</option>
+          <option value="Comedy">Comedy</option>
+          <option value="crime">Crime</option>
+          </select>
+      </div>)}
       <img
         src="https://www.ucl.ac.uk/news/sites/news/files/styles/large_image/public/ucl_quad_1.png?itok=fl4E7RXE"
         alt="University"
@@ -13,10 +22,8 @@ function Features() {
       <div className="info">
         <img  src="https://www.ucl.ac.uk/news/sites/news/files/styles/large_image/public/ucl_quad_1.png?itok=fl4E7RXE" alt="" />
         <span className="desc">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
-          iusto nisi facilis corrupti labore tempore exercitationem nesciunt
-          odit cum velit eius, porro maiores dicta neque commodi. Quis quaerat
-          debitis nulla.
+        loremadsafsjkjkdkjvdsjkdsjvkdsdskjhvdskvkdsv
+        hjddskjfkdsfjdskfdskdsjkbvbvjsdkvkdjbvkdjb
         </span>
         <div className="buttons">
         <button className="play">
