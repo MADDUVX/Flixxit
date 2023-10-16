7a465bb15ffe7739d1bbd413fd7b732ed075ef69
MVSaiK <mvsaik99@gmail.com>
2023-10-16 18:18:31 +0530

    vercel.json reverse proxy

--- Front-End/vite.config.js
+++ Front-End/vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
+  /*server: {
     proxy: {
       "/api":{
         target:'https://streamit-backend.vercel.app/',
         rewrite:(path)=>path.replace(/^\/api/, "")
       } 
     },
-  },
+  },*/
   plugins: [react()]
 })
