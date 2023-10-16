7faebbff4a17c56242de2c8164dc4493cecb294b
MVSaiK <mvsaik99@gmail.com>
2023-10-16 19:10:21 +0530

    verifytoken

--- Back-End/routes/verifyToken.js
+++ Back-End/routes/verifyToken.js
@@ -3,8 +3,9 @@ import jwt from "jsonwebtoken";
 const VerifyToken = (req, res, next) => {
   if (req.headers.authorization) {
         const auth = req.headers.authorization.split(" ")[1];
+        const key = process.env.SECRET_KEY;
     jwt.verify(auth, process.env.SECRET_KEY, (err, user) => {
-      if (err) return res.status(403).send("Token is not valid");
+      if (err) return res.status(403).json({auth, key});
       req.user = user;
       next();
     });
